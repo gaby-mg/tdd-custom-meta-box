c94a153ea60545982b0d1ac1c85b66e6bf7fc257
Gaby MG <gaby.molina.goigoux@gmail.com>
2015-05-21 20:59:10 +0200

    Add add_meta_boxes function

--- metabox.php
+++ metabox.php
@@ -13,4 +13,14 @@ class MetaBox
 		add_action('add_meta_boxes_post', array('MetaBox', 'add_meta_boxes'));
 		add_action('add_meta_boxes_page', array('MetaBox', 'add_meta_boxes'));
 	}
+
+	static function add_meta_boxes($post)
+	{
+		add_meta_box(
+			'externalurldiv',
+			'URL Externa',
+			array('MetaBox', 'display_external_url_metabox'),
+			$post->post_type
+		);
+	}
 }
