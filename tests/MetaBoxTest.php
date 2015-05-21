8050e0c0d2b97443fb26629cb021776fd07a5862
Gaby MG <gaby.molina.goigoux@gmail.com>
2015-05-21 20:28:38 +0200

    Refactor test to use init as a static method

--- tests/MetaBoxTest.php
+++ tests/MetaBoxTest.php
@@ -7,17 +7,16 @@ class MetaBoxTest extends PHPUnit_Framework_TestCase
 	function testHooksAFunctionToTheAddMetaBoxesAction()
 	{
 		global $wp_filter;  // We could have named it anything else
-		$meta_box = new MetaBox();
-		$meta_box->init();
+		MetaBox::init();
 
 		$this->assertSame(
 			$wp_filter['add_meta_boxes_post'],
-			array($meta_box, 'add_meta_boxes')
+			array('MetaBox', 'add_meta_boxes')
 		);
 		
 		$this->assertSame(
 			$wp_filter['add_meta_boxes_page'],
-			array($meta_box, 'add_meta_boxes')
+			array('MetaBox', 'add_meta_boxes')
 		);
 	}
 }
