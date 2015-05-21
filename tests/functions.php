482a9b5e19eaee58fc040e5ad2082a460827cfd6
Gaby MG <gaby.molina.goigoux@gmail.com>
2015-05-21 20:59:26 +0200

    Add add_meta_box function

--- tests/functions.php
+++ tests/functions.php
@@ -5,3 +5,9 @@ function add_action($tag, $function_to_add)
 	global $wp_filter;
 	$wp_filter[$tag] = $function_to_add;
 }
+
+function add_meta_box($id, $title, $callback, $screen)
+{
+	global $wp_meta_boxes;
+	$wp_meta_boxes[$screen] = $callback;
+}
