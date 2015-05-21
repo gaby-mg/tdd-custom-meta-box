9a836907ae519ef3dee00285ad64d40373f3f1fa
Gaby MG <gaby.molina.goigoux@gmail.com>
2015-05-21 20:04:03 +0200

    Require metabox.php file

--- tests/MetaBoxTest.php
+++ tests/MetaBoxTest.php
@@ -1,6 +1,22 @@
 <?php
+require 'metabox.php';
 
 class MetaBoxTest extends PHPUnit_Framework_TestCase
 {
+	function testHooksAFunctionToTheAddMetaBoxesAction()
+	{
+		global $wp_filter;  // We could have named it anything else
+		$meta_box = new MetaBox();
+		$meta_box->init();
 
+		$this->assertSame(
+			$wp_filter['add_meta_boxes_post'],
+			array($meta_box, 'add_meta_boxes')
+		);
+		
+		$this->assertSame(
+			$wp_filter['add_meta_boxes_page'],
+			array($meta_box, 'add_meta_boxes')
+		);
+	}
 }
